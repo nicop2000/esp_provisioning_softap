6a4536898aad17195cd0f56750dd4c3e487f83b3
Nico Petersen <petersen.nico@icloud.com>
2024-11-10 15:21:57 +0100

    some updates

--- example/android/app/src/main/kotlin/de/petersen/nico/esp_softap_provisioning_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.omert.esp_provisioning_softap_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/lib/wifi_screen/wifi.dart
+++
@@ -1,4 +0,0 @@
-export 'wifi_bloc.dart';
-export 'wifi_event.dart';
-export 'wifi_state.dart';
-export 'wifi_screen.dart';
